6c491b9eabcc0528f630abdd73399ad4bd44bfe8
TCGeneric <qoo0302@naver.com>
2018-11-29 12:44:16 +0900

    Slight changes

--- webapp/js/opacityanimation.js
+++ webapp/js/opacityanimation.js
@@ -3,9 +3,9 @@ var opacity = 1;
 function DoFadeOut() {
     if(opacity > 0) {
         opacity -= .1;
-        setTimeout(function(){DoFadeOut()}, 100);
+        setTimeout(function(){DoFadeOut()}, 150);
     } 
     document.getElementsByTagName("section")[0].style.opacity = opacity;
 }
 
-document.onscroll = DoFadeOut;
+window.onload = DoFadeOut;
